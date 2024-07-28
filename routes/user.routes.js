11f2f5fe8707134d30636670bb354f31d80f0b47
jahidulhasan1 <thejhr20@gmail.com>
2024-07-28 10:43:08 -0700

    crud added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,21 @@
 import express from "express";
-import { signUpController ,getAllUsers,logInController,logOutController,getMyProfile} from "../controllers/user.controllers.js";
+import {
+  getAllUsers,
+  signUpController,
+  logOutController,
+  logInController,
+  getMyProfile,
+} from "../controllers/user.controllers.js";
 import { isAuthenTicated } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 // register
-router.get("/all",getAllUsers);
-router.post("/signup",signUpController);
-router.post("/login",logInController);
-router.post("/logout",logOutController);
+router.get("/all", getAllUsers);
+router.post("/signup", signUpController);
+router.post("/login", logInController);
+router.post("/logout", logOutController);
 
-router.get("/me",isAuthenTicated, getMyProfile);
+router.get("/me", isAuthenTicated, getMyProfile);
 
 export default router;
